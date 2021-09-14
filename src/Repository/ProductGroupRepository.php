b391fcde8363b6c5cd2dadce3af728c393dd519d
ivan <vanyasanich@gmail.com>
2021-09-14 17:12:58 +0300

    Remade entities

---
+++ src/Repository/ProductGroupRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ProductGroup;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method ProductGroup|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ProductGroup|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ProductGroup[]    findAll()
+ * @method ProductGroup[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ProductGroupRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ProductGroup::class);
+    }
+}
--- src/Repository/StoreRepository.php
+++ src/Repository/StoreRepository.php
@@ -2,10 +2,12 @@
 
 namespace App\Repository;
 
+use App\Entity\Product;
 use App\Entity\Store;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Doctrine\Persistence\ManagerRegistry;
-
 /**
  * @method Store|null find($id, $lockMode = null, $lockVersion = null)
  * @method Store|null findOneBy(array $criteria, array $orderBy = null)
@@ -19,32 +21,106 @@ class StoreRepository extends ServiceEntityRepository
         parent::__construct($registry, Store::class);
     }
 
-    // /**
-    //  * @return Store[] Returns an array of Store objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @param string $name
+     *
+     * @return bool
+     *
+     * @throws NoResultException
+     * @throws NonUniqueResultException
+     */
+    public function existShopByName(string $name): bool
     {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder
+            ->select(
+                $queryBuilder->expr()->countDistinct('s.id')
+            )
+            ->where(
+                $queryBuilder->expr()->eq('s.name', ':name')
+            )
+            ->setParameter('name', $name)
         ;
-    }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Store
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $queryBuilder->getQuery()->getSingleScalarResult() > 0;
     }
-    */
+
+    /**
+     * @param Product $product
+     *
+     * @return Store[]|array
+     */
+    public function findStoresByProduct(Product $product): array
+    {
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder
+            ->leftJoin('s.inventories', 'i')
+            ->leftJoin('i.product', 'p')
+            ->andWhere(
+                $queryBuilder->expr()->eq('p.id', ':product')
+            )
+            ->andWhere(
+                $queryBuilder->expr()->gt('i.quantity', 0)
+            )
+            ->setParameter('product', $product)
+        ;
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * @param Product $product
+     *
+     * @return array
+     */
+    public function findPriorityStoresProductQuantityByProduct(Product $product): array
+    {
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder
+            ->select([
+                'i.quantity as quantity'
+            ])
+            ->leftJoin('s.inventories', 'i')
+            ->leftJoin('i.product', 'p')
+            ->andWhere(
+                $queryBuilder->expr()->eq('p.id', ':product')
+            )
+            ->andWhere(
+                $queryBuilder->expr()->gt('i.quantity', 0)
+            )
+            ->orderBy('s.priority', 'ASC')
+            ->setParameter('product', $product)
+        ;
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * @param Product $product
+     * @param int $limit
+     *
+     * @return Store[]|array
+     */
+    public function findPriorityStoresIdsByLimit(Product $product, int $limit): array
+    {
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder
+            ->select([
+                's.id as id',
+            ])
+            ->leftJoin('s.inventories', 'i')
+            ->leftJoin('i.product', 'p')
+            ->andWhere(
+                $queryBuilder->expr()->eq('p.id', ':product')
+            )
+            ->andWhere(
+                $queryBuilder->expr()->gt('i.quantity', 0)
+            )
+            ->orderBy('s.priority', 'ASC')
+            ->setParameter('product', $product)
+            ->setMaxResults($limit)
+        ;
+
+        return $queryBuilder->getQuery()->getResult();
+    }
 }
